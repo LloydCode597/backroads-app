da5eef8d4bbc74a53961363e63ad823cf237f6b3
Lloyd Allen <lloydallen597@gmail.com>
2023-05-27 20:52:45 -0500

    Added the page and Social Links to the data file

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const pageLinks = [
+  { id: 1, herf: "#home", text: "home" },
+  { id: 2, herf: "#about", text: "about" },
+  { id: 3, herf: "#services", text: "serices" },
+  { id: 4, herf: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  { id: 1, herf: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, herf: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, herf: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
